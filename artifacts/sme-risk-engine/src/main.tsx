765133a047fc9962e26870834036a005a476887a
Occumed79 <Alex.Ayvazian@logan.edu>
2026-08-09 21:15:32 -0700

    Load Tahoe liquid glass material layer last

--- artifacts/sme-risk-engine/src/main.tsx
+++ artifacts/sme-risk-engine/src/main.tsx
@@ -3,5 +3,6 @@ import App from "./App";
 import "./index.css";
 import "./pages/workbench.css";
 import "./reviewer-polish.css";
+import "./tahoe-liquid-glass.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
