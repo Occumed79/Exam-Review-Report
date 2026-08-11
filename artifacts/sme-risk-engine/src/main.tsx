faa08f6ebd371d0ff69460b207d00f6f63535b5a
Occumed79 <Alex.Ayvazian@logan.edu>
2026-08-11 01:11:22 -0700

    Load alpine UI theme overrides

--- artifacts/sme-risk-engine/src/main.tsx
+++ artifacts/sme-risk-engine/src/main.tsx
@@ -3,5 +3,6 @@ import App from "./App";
 import "./index.css";
 import "./pages/workbench.css";
 import "./tahoe-liquid-glass.css";
+import "./alpine-ui-theme.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
