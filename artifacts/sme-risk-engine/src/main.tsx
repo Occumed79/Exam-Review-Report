6f5329e0119f80d2d2f769848ac2b2bf168d1775
Occumed79 <Alex.Ayvazian@logan.edu>
2026-08-11 21:48:28 -0700

    Prune unreachable legacy engines

--- artifacts/sme-risk-engine/src/main.tsx
+++ artifacts/sme-risk-engine/src/main.tsx
@@ -1,10 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./pages/workbench.css";
-import "./tahoe-liquid-glass.css";
-import "./alpine-ui-theme.css";
-import "./palette-balance.css";
-import "./workstation-completion.css";
+import "./app-theme.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
