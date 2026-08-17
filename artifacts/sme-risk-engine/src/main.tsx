feeb198ff5bc6f2c09f0fddd83f5d0e5f4c29a37
Occumed79 <Alex.Ayvazian@logan.edu>
2026-08-16 23:14:15 -0700

    Load advanced workstation visuals across reviewer tabs

--- artifacts/sme-risk-engine/src/main.tsx
+++ artifacts/sme-risk-engine/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./app-theme.css";
+import "./pages/advanced-tab-system.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
